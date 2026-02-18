d370973e57aabc1da170c5f855db7a5c89d7b2d5
cherryhubluascript-ship-it <cherryhubluascript@gmail.com>
2026-02-18 15:56:31 -0500

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+let serverData = { placeId: null, jobId: null };
+
+app.post("/set-server", (req, res) => {
+    const { placeId, jobId } = req.body;
+    serverData.placeId = placeId;
+    serverData.jobId = jobId;
+    res.json({ status: "saved" });
+});
+
+app.get("/get-server", (req, res) => {
+    res.json(serverData);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running"));
